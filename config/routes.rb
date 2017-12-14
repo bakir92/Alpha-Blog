aa3a8a45ecfffadb8c5a06d155db04c61890ca5f
Bakir Sharaf <bakirts92@gmail.com>
2017-12-14 20:06:08 +0530

    Article views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+	resources :articles
 	get 'welcome/home', to: 'welcome#home'
 	get 'welcome/about', to: 'welcome#about'
 
 	root 'pages#home'
 	get 'about', to: 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
